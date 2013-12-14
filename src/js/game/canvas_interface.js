6f596a5be2d50ccb92c35a713f4ba920e9414586
Dominic May <jack.thatch@gmail.com>
2013-12-15 01:39:50 +0800

    Sorta fixes it, but only if the image is the right size

--- src/js/game/canvas_interface.js
+++ src/js/game/canvas_interface.js
@@ -18,9 +18,7 @@ CanvasInterface = Class.extend({
         'use strict';
         this._context.drawImage(
             this._images[name],
-            0, 0,
-            this.width(),
-            this.height()
+            0, 0
         );
     },
 
