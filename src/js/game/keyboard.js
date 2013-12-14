7942c02c69e5b303f4e2d14e5b89c4c25888b20c
Dominic May <jack.thatch@gmail.com>
2013-12-14 22:05:03 +0800

    Added Keyboard class, mostly stolen from ThatOtherPerson's practice platformer

---
+++ src/js/game/keyboard.js
@@ -0,0 +1,46 @@
+var Keyboard;
+
+Keyboard = Class.extend({
+    left: false,
+    right: false,
+    jump: false,
+
+    init: function(){},
+
+    connect: function(){
+        window.onkeydown = $.proxy(this.onkeydown, this);
+        window.onkeyup = $.proxy(this.onkeyup, this);
+    },
+
+    onkeydown: function(e) {
+        var key = e.keyCode ? e.keyCode : e.which;
+        switch (key)
+        {
+            case 37: // left
+                this.left = true;
+                break;
+            case 39: // right
+                this.right = true;
+                break;
+            case 32: // space
+                this.jump = true;
+                break;
+        }
+    },
+
+    onkeyup: function(e) {
+        var key = e.keyCode ? e.keyCode : e.which;
+        switch (key)
+        {
+            case 37: // left
+                this.left = false;
+                break;
+            case 39: // right
+                this.right = false;
+                break;
+            case 32: // space
+                this.jump = false;
+                break;
+        }
+    }
+});
