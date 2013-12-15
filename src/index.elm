d2921cf9f6b84285f1e226898b76f28776f42f34
nvd1234@gmail.com <nvd1234@gmail.com>
2013-12-15 20:03:55 +0000

    Added rotation effect among probably other things

--- src/index.elm
+++ src/index.elm
@@ -8,7 +8,7 @@ wobble = foldp (\_ -> not) False <| every (500 * millisecond)
 chooseImg c = if c then "img/forward2.png" else "img/forward1.png"
 
 display : Signal Element
-display = image 300 500 . chooseImg <~ wobble
+display = collage 900 500 . (\x->[x]) <~ (rotate <~ rotationAngle ~ (toForm . image 300 500 . chooseImg <~ wobble))
 
 tiltInfo : Signal Element
 tiltInfo = centered . toText . (++) "Tilt (keep between -1 and 1): " . show <~ tilt
@@ -17,13 +17,16 @@ driftInfo : Signal Element
 driftInfo = centered . toText . (++) "Drift (keep between -10 and 10): " . show <~ drift
 
 pedalStep : Bool -> Float
-pedalStep b = if b then 0.2 else -0.2
+pedalStep b = if b then -0.2 else 0.2
 
 leaning : Signal Float
-leaning = (*) 0.25 . toFloat . .x <~ sampleOn wobble Keyboard.arrows
+leaning = (*) 0.25 . toFloat . .x <~ sampleOn (every (50 * millisecond)) Keyboard.arrows
 
 tilt : Signal Float
-tilt = foldp (+) 0 <| (+) . pedalStep <~ wobble ~ leaning
+tilt = lift ((*) 0.1) <| foldp (+) 0 <| (+) . pedalStep <~ wobble ~ leaning
+
+rotationAngle : Signal Float
+rotationAngle = asin . flip (/) (0-sqrt 2) <~ tilt
 
 drift : Signal Float
 drift = foldp (+) 0 tilt
