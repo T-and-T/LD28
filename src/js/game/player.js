9480e7cdbad79a14d5393534c4339066de6d7b34
Dominic May <jack.thatch@gmail.com>
2013-12-15 15:21:37 +0800

    Add support for the Player sprite to face a direction

--- src/js/game/player.js
+++ src/js/game/player.js
@@ -3,6 +3,7 @@ var Player;
 Player = Entity.extend({
     max_x: null,
     max_y: null,
+    direction: null,
 
     init: function(max_x, max_y){
         this.max_x = max_x;
@@ -17,12 +18,25 @@ Player = Entity.extend({
         return "<Player x=" + this.x + ", y=" + this.y + ">";
     },
 
+    currentSpriteName: function(){
+        if (this.direction === LEFT || this.direction === null) {
+            return 'player_left';
+        } else if (this.direction === RIGHT) {
+            return 'player_right';
+        }
+    },
+
     // key_info: _.debounce(function(kb_states){
     key_info: function(kb_states) {
         var x = 0, y = 0;
 
-        if(kb_states[LEFT]) { x = 1; }
-        else if (kb_states[RIGHT]) { x = -1; }
+        if(kb_states[LEFT]) {
+            x = 1;
+            this.direction = LEFT;
+        } else if (kb_states[RIGHT]) {
+            x = -1;
+            this.direction = RIGHT;
+        }
 
         if(kb_states[UP]) { y = 1; }
         else if (kb_states[DOWN]) { y = -1; }
